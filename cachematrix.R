2bfb86609f2fee96b52c47a179375b5c48f5f376
Richard Kwon <richardkwon@hotmail.com>
2017-04-09 21:51:10 -0500

    Coursera: R-Programming: Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+##############################################################################
+## Name: Richard Kwon
+## Course: R-Programming:Lexical Scoping Assignment
+##
+## Matrix Inversion
+## A pair of functions that will cache the inverse of a matrix
+##############################################################################
 
-## Write a short comment describing this function
-
+## function::makeCacheMatrix
+## creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## initialize inverse matrix variable
+    ## this variable will store the inverse matrix of x
+    inv_matrix <- NULL
+    
+    set <- function(y) {
+        x <<- y                   ## assign y (passed in) to x
+        inv_matrix <<- NULL       ## set inverse matrix to NULL
+    }
+    get <- function () x          ## return the current value of x
+    
+    setinvmatrix <- function(m) inv_matrix <<- m  ## set inverse matrix value
+    getinvmatrix <- function() inv_matrix         ## get inverse matrix value
+    
+    list(set=set, get=get, setinvmatrix=setinvmatrix, getinvmatrix=getinvmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## function: cacheSolve
+## returns the inverse of a matrix either from cache or thru function solve() call
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinvmatrix()         ## get the inverse matrix value
+    
+    if (!is.null(m)) {            ## if inverse matrix value is NOT NULL then
+        print ("***** PRINTING FROM CACHE *****")
+        return (m)
+    }
+    
+    data <- x$get()               ## get the x matrix
+    m <- solve(data)              ## get the inverse matrix of x
+    x$setinvmatrix(m)             ## set the calculated inverse matrix value
+    m
 }
